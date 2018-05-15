d31783b8388bf85d34744997cb9d0780353f7e14
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-05-14 23:03:41 -0400

    current word now shows dashes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # word-guess-game
 
 https://gibbsk12.github.io/word-guess-game/
+
+1. Display Current Word with Spaces
+2. Show Number of Guesses Remaining
+      --Do not let the same be guessed twice
+3. Show Letters 
+4. Show Number of Wins
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,22 +24,33 @@ var words = [
     "stupefy",
 ]
 
-// Chooses a Word from Array
-var chosenWord = words[Math.floor(Math.random() * words.length)]
-console.log(chosenWord);
-
 // Sets Game Variables
 guessesLeft = 10; //Number of tries a player has to guess the word 
 previousGuesses = []; //Letters already guessed
 answerArray = []; //The dashes and letters guessed
 winNumber = []; //Number of Wins 
 
+// Chooses a Word from Array
+var chosenWord = words[Math.floor(Math.random() * words.length)]
+console.log(chosenWord);
+
 // Sets up Number of Blanks Needed
 var numberOfBlanks = chosenWord.length;
 console.log(numberOfBlanks);
 
-for (i = 0; i < numberOfBlanks; i++) {
+for (var i = 0; i < numberOfBlanks; i++) {
     answerArray.push("_");
-    console.log(answerArray);
+    // console.log(answerArray);
 }
 
+var currentWordState = "";
+
+for (var i = 0; i < answerArray.length; i++) {
+    currentWordState += answerArray[i] + " ";
+
+}
+
+
+document.getElementById("currentWord").innerHTML = currentWordState;
+
+// answerArray;
