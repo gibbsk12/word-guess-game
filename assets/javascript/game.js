fac10175edc892e1ed15b071f2ce1f562b8a8104
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-05-17 22:45:37 -0400

    commenting what pieces of code should do

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,42 +28,42 @@ var guessesLeft = 10; //Number of tries a player has to guess the word
 var previousGuesses = []; //Letters already guessed
 var answerArray = []; //The dashes and letters guessed
 var winNumber = 0; //Number of Wins 
-var chosenWord;
-var wordSpace = document.getElementById("currentWord");
-var letterGuesses = document.getElementById("previousGuesses");
-var guessesRemaining = document.getElementById("numberOfGuessesLeft");
-var winCount = document.getElementById("numberOfWins");
-var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-// Chooses a Word from Array
+var chosenWord; //The word that is chosen
+var wordSpace = document.getElementById("currentWord"); //Grabbing the span for answers
+var letterGuesses = document.getElementById("previousGuesses"); //Grabbing the previous guesses 
+var guessesRemaining = document.getElementById("numberOfGuessesLeft"); //Grabbing guesses paragraph
+var winCount = document.getElementById("numberOfWins"); //Grabbing number of wins paragraph
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //Setting up variable so only alphabet letters can be used 
 
-// 
+
+// When a new game starts, sets variables 
 function startNewGame() {
+    //Resets variables
     answerArray = [];
     previousGuesses = [];
     guessesLeft = 10;
-
+    //Chooses a random word 
     chosenWord = words[Math.floor(Math.random() * words.length)]
     console.log(chosenWord);
-
-    // Sets up Number of Blanks Neede
-
+    // Sets up Number of Blanks Needed
     for (var i = 0; i < chosenWord.length; i++) {
         answerArray.push("_");
         console.log(answerArray);
         wordSpace.innerHTML = answerArray.join(" ");
     }
-
+    //Rewrites variables
     letterGuesses.innerHTML = "Previous Guesses: " + previousGuesses;
     guessesRemaining.innerHTML = "Number of Guesses: " + guessesLeft;
-    
 }
 
 function updateWords(letter) {
+    //If the letter does not match, then letter is added to previous guesses and a turn is lost.
     if (chosenWord.indexOf(letter) === -1 && alphabet.indexOf(letter) >= 0) {
         previousGuesses.push(letter);
         letterGuesses.innerHTML = "Previous Guesses: " + previousGuesses.join(", ");
         guessesLeft--;
         guessesRemaining.innerHTML = "Number of Guesses: " + guessesLeft;
+    //If the letter matches, then letter is added to answerArray
     } else if (chosenWord.indexOf(letter) >= 0 && alphabet.indexOf(letter) >= 0) {
         for (var i = 0; i < chosenWord.length; i++) {
             if (chosenWord[i] === letter) {
@@ -75,12 +75,14 @@ function updateWords(letter) {
 
 }
 
-function youWon () {
+//Adds to the number of wins 
+function youWon() {
     winNumber++;
     winCount.innerHTML = "Number of Wins: " + winNumber;
 
 }
 
+//Alerts user if they won or lost
 function didYouWin() {
     if (answerArray.indexOf("_") === -1) {
         alert("You're a word wizard!");
@@ -101,4 +103,5 @@ document.onkeyup = function (event) {
     console.log(humanInput);
 }
 
+//What happens when the page loads 
 window.onload = startNewGame();
